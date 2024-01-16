bf66b56f1d89e2ac904dce7c444874e722c97025
Marifer Villalobos <mfvl.1186@gmail.com>
2024-01-15 22:24:37 -0300

    Create navbar

--- app/page.js
+++ app/page.js
@@ -6,24 +6,9 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Probando Ando;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+        
       </div>
       <p>
-      <Link href="/about">Acerca de mi</Link>
-      <Link href="/services">Servicios</Link>
-      <Link href="/services/share">Compartir</Link>
-      <Link href="/contact">Contacto</Link>
-      <Link href="/testimony">Testimonios</Link>
-      <Link href="/resources">Recursos Descargables</Link>
-      <Link href="/gallery">Galeria</Link>
-      <Link href="/faq">Preguntas Frecuentes</Link>
-      <Link href="/blog">Blog</Link>
-
-
-
       </p>
     </main>
   )
---
+++ components/main-header.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoHorizontal from '@/assets/logoHorizontal.png'
+import classes from '@/components/main-header.module.css'
+
+export default function MainHeader() {
+    return (
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <Image src={logoHorizontal} alt="Logo de Escuela Sexualidad Sagrada color amarillo y rojo" />
+                Escuela de Sexualidad Sagrada Latinoamericana
+            </Link>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href="/about">Acerca de mi</Link>
+                    </li>
+                    <li>
+                        <Link href="/services">Servicios</Link>
+                    </li>
+                    <li>
+                        <Link href="/services/share">Compartir</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">Contacto</Link>
+                    </li>
+                    <li>
+                        <Link href="/testimony">Testimonios</Link>
+                    </li>
+                    <li>
+                        <Link href="/resources">Recursos Descargables</Link>
+                    </li>
+                    <li>
+                        <Link href="/gallery">Galeria</Link>
+                    </li>
+                    <li>
+                        <Link href="/faq">Preguntas Frecuentes</Link>
+                    </li>
+                    <li>
+                        <Link href="/blog">Blog</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+
+}
