4710b3030b83759c057ed985a84187185c47714d
Marifer Villalobos <mfvl.1186@gmail.com>
2024-01-29 00:23:36 -0300

    Add responsive uderconstruction component

--- components/underConstruction/index.js
+++ components/underConstruction/index.js
@@ -1,16 +1,34 @@
+"use client";
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
-const UnderConstruction = () => {
-    return (
-        <div>
-            <Image
-                src="/images/UnderConstruction.png"
-                alt="Under Construction"
-                width={1920}
-                height={1080}
-            />
-        </div>
-    );
-};
+export default function UnderConstruction() {
+  const [windowWidth, setWindowWidth] = useState(0);
 
-export default UnderConstruction;
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Agrega el listener para escuchar cambios en el tamaño de la pantalla
+    window.addEventListener('resize', handleResize);
+
+    // Limpia el listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowWidth]);
+
+  const isLargeScreen = windowWidth >= 1024;
+
+  return (
+    <div>
+      <Image
+        src={isLargeScreen ? '/images/UnderConstructionlg.png' : '/images/UnderConstructionsm.png'}
+        alt="Under Construction"
+        width={1920}
+        height={1080}
+      />
+    </div>
+  );
+}
