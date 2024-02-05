7c0b0c44cdd6b927c49b3ecf8e61133099dbdd59
Marifer VL <mfvl.1186@gmail.com>
2024-02-05 02:36:59 -0300

    Fix sidebar links open/close function

--- components/hoverlink.js
+++ components/hoverlink.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
 
-export default function HoverLink({ href, text, title, hoverText, activeText, ariaLabel }) {
+export default function HoverLink({ href, text, title, hoverText, activeText, ariaLabel, closeSidebarOnClick }) {
+    const handleClick = () => {
+        closeSidebarOnClick && closeSidebarOnClick();
+    };
+
     return (
         <Link
             href={href}
@@ -9,6 +13,7 @@ export default function HoverLink({ href, text, title, hoverText, activeText, ar
             data-active={activeText}
             aria-label={ariaLabel}
             className="relative inline-block py-2 text-center text-sm font-semibold overflow-hidden group lg:w-32"
+            onClick={handleClick}
         >
             <span className="base-text">{text}</span>
             <span className="hover-text">{hoverText}</span>
--- components/navigation/Sidebar.js
+++ components/navigation/Sidebar.js
@@ -5,12 +5,10 @@ import HoverLink from '../hoverlink';
 import Logo from './Logo';
 
 export default function Sidebar({ closeSidebar }) {
-    console.log('closeSidebar: ', closeSidebar);
     const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         setIsOpen(true);
-        console.log('isOpen: ', isOpen);
 
     }, []);
 
@@ -20,7 +18,7 @@ export default function Sidebar({ closeSidebar }) {
     };
 
     return (
-        <div class="flex h-screen antialiased text-gray-900 dark:bg-dark dark:text-light">
+        <div className="flex h-screen antialiased text-gray-900 dark:bg-dark dark:text-light">
 
             <div
                 className={`fixed inset-y-0 z-20 flex w-80 ${isOpen ? 'group-hover:transform translate-x-0' : 'group-hover:transform -translate-x-full'}`}
@@ -65,6 +63,7 @@ export default function Sidebar({ closeSidebar }) {
                                     hoverText="Ingresa"
                                     activeText="Ingresando"
                                     ariaLabel="Ir a sección acerca de mi"
+                                    closeSidebarOnClick={handleCloseSidebar}
                                 />
                             </li>
 
@@ -76,6 +75,7 @@ export default function Sidebar({ closeSidebar }) {
                                     hoverText="Ingresa"
                                     activeText="Ingresando"
                                     ariaLabel="Ir a la página de Servicios"
+                                    closeSidebarOnClick={handleCloseSidebar}
                                 />
                             </li>
 
@@ -87,6 +87,7 @@ export default function Sidebar({ closeSidebar }) {
                                     hoverText="Ingresa"
                                     activeText="Ingresando"
                                     ariaLabel="Ir a la página de Testimonios"
+                                    closeSidebarOnClick={handleCloseSidebar}
                                 />
                             </li>
                             <li>
@@ -97,6 +98,7 @@ export default function Sidebar({ closeSidebar }) {
                                     hoverText="Ingresa"
                                     activeText="Ingresando"
                                     ariaLabel="Ir a la página de Descargables Espirituales"
+                                    closeSidebarOnClick={handleCloseSidebar}
                                 />
                             </li>
                             {/* <li>
@@ -117,6 +119,7 @@ export default function Sidebar({ closeSidebar }) {
                                     hoverText="Ingresa"
                                     activeText="Ingresando"
                                     ariaLabel="Ir a la página de Preguntas Frecuentes"
+                                    closeSidebarOnClick={handleCloseSidebar}
                                 />
                             </li>
                             {/* <li>
@@ -132,11 +135,22 @@ export default function Sidebar({ closeSidebar }) {
                             <li>
                                 <div className="flex mt-8 gap-2 text-primary">
                                     <Link
+                                        href="https://api.whatsapp.com/send?phone=56990897824&text=Hola,%20quiero%20recibir%20informaci%C3%B3n%20de%20la%20Escuela%20de%20Sexualidad%20Sagrada%20Latinoamericana,%20gracias"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        title="Escríbeme en Whatsapp"
+                                        aria-label="Escribir a Valeria del Real en Whatsapp"
+                                        onClick={handleCloseSidebar}
+                                    >
+                                        <FaWhatsapp className="text-xl text-primary transition duration-300" />
+                                    </Link>
+                                    <Link
                                         href="https://www.instagram.com/valeriadelreald"
                                         target="_blank"
                                         rel="noopener noreferrer"
                                         title="Sígueme en Instagram"
                                         aria-label="Ir a Instagram de Valeria del Real"
+                                        onClick={handleCloseSidebar}
                                     >
                                         <FaInstagram className="text-xl text-primary transition duration-300" />
                                     </Link>
@@ -146,6 +160,7 @@ export default function Sidebar({ closeSidebar }) {
                                         rel="noopener noreferrer"
                                         title="Sígueme en Youtube"
                                         aria-label="Ir a Youtube de Valeria del Real"
+                                        onClick={handleCloseSidebar}
                                     >
                                         <FaYoutube className="text-xl text-primary hover:scale-125 active:text-yellow-200 transition duration-300" />
                                     </Link>
@@ -155,6 +170,7 @@ export default function Sidebar({ closeSidebar }) {
                                         rel="noopener noreferrer"
                                         title="Sígueme en TikTok"
                                         aria-label="Ir a TikTok de Valeria del Real"
+                                        onClick={handleCloseSidebar}
                                     >
                                         <FaTiktok className="text-xl text-primary transition duration-300" />
                                     </Link>
@@ -164,17 +180,9 @@ export default function Sidebar({ closeSidebar }) {
                                         rel="noopener noreferrer"
                                         title="Sígueme en Facebook"
                                         aria-label="Ir a Facebook de Valeria del Real"
+                                        onClick={handleCloseSidebar}
                                     >
                                         <FaFacebook className="text-xl text-primary transition duration-300" />
-                                    </Link>
-                                    <Link
-                                        href="https://api.whatsapp.com/send?phone=56990897824&text=Hola,%20quiero%20recibir%20informaci%C3%B3n%20de%20la%20Escuela%20de%20Sexualidad%20Sagrada%20Latinoamericana,%20gracias"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        title="Escríbeme en Whatsapp"
-                                        aria-label="Escribir a Valeria del Real en Whatsapp"
-                                    >
-                                        <FaWhatsapp className="text-xl text-primary transition duration-300" />
                                     </Link>
                                 </div>
                             </li>
