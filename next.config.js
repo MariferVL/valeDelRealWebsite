f1dbe269d2064ee4d9ae5be91034af11d4a75e77
MariferVL <mfvl.1186@gmail.com>
2025-07-30 22:56:25 -0400

    Update congig files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
   images: {
-    loader: 'default', 
-    formats: ['image/avif', 'image/webp'], 
+    unoptimized: true,
   },
 };
 
