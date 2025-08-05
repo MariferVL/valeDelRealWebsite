f6274074b77149e3d1f3c3af77dac8843fc309f2
MariferVL <mfvl.1186@gmail.com>
2025-08-04 23:40:47 -0400

    Improve A11y details

--- components/layout/SimpleFooter.js
+++ components/layout/SimpleFooter.js
@@ -1,34 +1,67 @@
-// components/SimpleFooter.js
-
-import Link from 'next/link';
-import { FaInstagram, FaYoutube, FaTiktok, FaFacebook, FaWhatsapp } from 'react-icons/fa';
+import Link from "next/link";
+import {
+  FaInstagram,
+  FaYoutube,
+  FaTiktok,
+  FaFacebook,
+  FaWhatsapp,
+} from "react-icons/fa";
 
 export default function SimpleFooter() {
   return (
-    // AÑADIMOS CLASES para posicionarlo de forma absoluta en la parte inferior
     <footer className="absolute bottom-0 left-0 w-full z-10 p-4">
       <div className="w-full text-white flex flex-col items-center">
         <div className="flex space-x-4">
-          <Link href="https://api.whatsapp.com/send?phone=56990897824&text=Hola,%20quiero%20recibir%20informaci%C3%B3n%20de%20la%20Escuela%20de%20Sexualidad%20Sagrada%20Latinoamericana,%20gracias" target="_blank" rel="noopener noreferrer">
+          <Link
+            href="https://api.whatsapp.com/send?phone=56990897824&text=Hola,%20quiero%20recibir%20informaci%C3%B3n%20de%20la%20Escuela%20de%20Sexualidad%20Sagrada%20Latinoamericana,%20gracias"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Contáctanos en WhatsApp"
+          >
             <FaWhatsapp className="text-xl text-white hover:text-yellow-400 transition duration-300" />
           </Link>
-          <Link href="https://www.instagram.com/valeriadelreald" target="_blank" rel="noopener noreferrer">
+          <Link
+            href="https://www.instagram.com/valeriadelreald"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Síguenos en Instagram"
+          >
             <FaInstagram className="text-xl text-white hover:text-yellow-400 transition duration-300" />
           </Link>
-          <Link href="https://www.youtube.com/@escueladesexualidadsSgrada" target="_blank" rel="noopener noreferrer">
+          <Link
+            href="https://www.youtube.com/@escueladesexualidadsSgrada"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Síguenos en YouTube"
+          >
             <FaYoutube className="text-xl text-white hover:text-yellow-400 transition duration-300" />
           </Link>
-          <Link href="https://www.tiktok.com/@valeriadelreald" target="_blank" rel="noopener noreferrer">
+          <Link
+            href="https://www.tiktok.com/@valeriadelreald"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Síguenos en TikTok"
+          >
             <FaTiktok className="text-xl text-white hover:text-yellow-400 transition duration-300" />
           </Link>
-          <Link href="https://web.facebook.com/valeria.d.9?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer">
+          <Link
+            href="https://web.facebook.com/valeria.d.9?_rdc=1&_rdr"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Síguenos en Facebook"
+          >
             <FaFacebook className="text-xl text-white hover:text-yellow-400 transition duration-300" />
           </Link>
         </div>
         <hr className="border-primary w-2/3 mt-3" />
         <p className="w-full text-center mt-2 text-xs lg:text-base text-white">
           Desarrollado y diseñado con amor por{" "}
-          <Link href="https://github.com/MariferVL" target="_blank" rel="noopener noreferrer">
+          <Link
+            href="https://github.com/MariferVL"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Visita perfil de GitHub MariferVL"
+          >
             <strong>MariferVL</strong>
           </Link>
           <br />
